e123e1e3bfe8946ec1eca0c8c3713d67b652d63e
AllenYGY <allenygy@outlook.com>
2024-05-16 15:21:54 +0800

    更新B站爬虫脚本

--- get_comment_score.py
+++ get_comment_score.py
@@ -2,11 +2,11 @@ from snownlp import SnowNLP, sentiment
 
 
 def get_comment_score(comment):
-    sentiment.load("libs/Model/MovieComment.marshal")
+    sentiment.load("model/MovieComment.marshal")
     s = SnowNLP(comment)
     return s.sentiments
 
 if __name__ == "__main__":    
-    print(get_comment_score("这个电影真的太好看了！"))
-    print(get_comment_score("byd"))
-    print(get_comment_score("今天天气真好！"))
+    print("这个电影真的太好看了！",get_comment_score("这个电影真的太好看了！"))
+    print("byd,真唐",get_comment_score("byd,真唐"))
+    print("今天天气真好！",get_comment_score("今天天气真好！"))
